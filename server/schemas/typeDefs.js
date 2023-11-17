fd204b27a29227e6e72aa428ee331bef2d30ba3b
Pixls112 <nguyen.phillip112@gmai.com>
2023-11-16 18:27:50 -0800

    added basic typeDef and created a resolvers.js file

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,25 @@
+const typeDefs = `
+    type Tournament {
+        _id: ID
+        tournName: String
+        playerSize: Int
+        players: [Player]
+        join : Boolean
+        full : Boolean
+    }
+
+    type Player {
+        _id: ID
+        name: String
+    }
+
+    type Query {
+        tournament(_id: ID): Tournament
+        player(_id: ID): Player
+    }
+
+    
+
+`
+module.exports = typeDefs;
+
