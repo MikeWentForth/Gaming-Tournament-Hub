ae04cb2a3501c106219f2598fe9efec1233749da
JavierBurgara <javierburgara150@yahoo.com>
2023-11-27 21:33:19 -0800

    new changes

--- client/src/components/TournamentInfo/index.jsx
+++ client/src/components/TournamentInfo/index.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-function TournamentResult({ tournamentName, game, onViewClick }) {
+function TournamentResult({ tournamentName, game, onViewClick, key }) {
   return (
-    <div className="tournament-result">
+    <div key={key} className="tournament-result">
       <h3>{tournamentName}</h3>
       <p>Game: {game}</p>
       {/* <Link to={`tournaments/${id}`}><button onClick={onViewClick}>View</button></Link> */}
