ffdff8e58a26643ca8fab9d3a5520ad034f6ee0c
CJFeagin33 <135062039+CJFeagin33@users.noreply.github.com>
2023-11-28 17:28:21 -0800

    added tournament info cards to landing/join page

--- client/src/components/TournamentInfo/index.jsx
+++ client/src/components/TournamentInfo/index.jsx
@@ -1,10 +1,11 @@
 /* eslint-disable react/prop-types */
 import { Link } from 'react-router-dom';
+import './index.css'
 
 function TournamentResult({tournamentId, tournamentName, game, onViewClick }) {
   console.log(`THis is the KEY::: ${tournamentId}`);
   return (
-    <div>
+    <div className='tournament-result'>
       <h3>{tournamentName}</h3>
       <p>Game: {game}</p>
       <Link to={`/tournaments/${tournamentId}`}><button onClick={onViewClick}>View</button></Link>
