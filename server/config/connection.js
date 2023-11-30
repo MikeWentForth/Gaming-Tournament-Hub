eb83ec2ea34858471647ca31bde0f597592043a6
matthew <matthkang@gmail.com>
2023-11-29 19:04:30 -0800

    connection url

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/tournaments');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://0.0.0.0:27017/tournaments');
 
 module.exports = mongoose.connection;
